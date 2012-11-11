27d4a41b722d34fa930ebb7a2f9052afae5b826a
striker <albinin0002@gmail.com>
2012-11-11 13:15:23 +0530

    Database update admin added

--- localhost.sql
+++ localhost.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
+-- version 3.4.5
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 10, 2012 at 02:43 PM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
+-- Host: localhost
+-- Generation Time: Nov 11, 2012 at 08:42 AM
+-- Server version: 5.5.16
+-- PHP Version: 5.3.8
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -39,7 +39,14 @@ CREATE TABLE IF NOT EXISTS `member` (
   PRIMARY KEY (`Id`),
   UNIQUE KEY `MId` (`MId`),
   UNIQUE KEY `Email` (`Email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`Id`, `MId`, `FirstName`, `LastName`, `PhoneNumber`, `Email`, `Type`) VALUES
+(1, 'Q3GVXs', '', '', 0, 'admin@localhost', 'Station');
 
 -- --------------------------------------------------------
 
@@ -53,7 +60,14 @@ CREATE TABLE IF NOT EXISTS `opensesame` (
   `AccessCode` varchar(50) NOT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `MId` (`MId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `opensesame`
+--
+
+INSERT INTO `opensesame` (`Id`, `MId`, `AccessCode`) VALUES
+(1, 'Q3GVXs', 'a1769e88fb73465b284139add2b0b9c1488c6b61');
 
 -- --------------------------------------------------------
 
@@ -72,14 +86,15 @@ CREATE TABLE IF NOT EXISTS `profile` (
   `ProfileImage` varchar(100) NOT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `MId` (`MId`,`ProfileImage`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `profile`
 --
 
 INSERT INTO `profile` (`Id`, `MId`, `FirstName`, `LastName`, `EmploymentDate`, `JoiningDate`, `Age`, `ProfileImage`) VALUES
-(1, '', 'Test', 'Test2', NULL, '2012-10-30', 15, '');
+(1, '', 'Test', 'Test2', NULL, '2012-10-30', 15, ''),
+(2, 'Q3GVXs', '', '', NULL, '0000-00-00', 0, '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
